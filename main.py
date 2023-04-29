ad89e67a1d5536e8dbc5b8d86e2a305a2db88022
Jeff Hauck <jefferyhauckjr@gmail.com>
2023-04-28 21:00:17 -0700

    further-integration

--- main.py
+++ main.py
@@ -1,139 +1,8 @@
-# import requests
-# import PySimpleGUI as sg
-
-# URL = "https://pokeapi.co/api/v2/"
-
-
-
-
-# def overview():
-#     welcome = input(
-#         "Welcome to the world of pokemon. Using a keyword, what are you looking for? ").lower()
-#     print(welcome)
-#     response = requests.get(URL).json()
-
-#     keys = [key.split()[0] for key in response.keys()]
-
-#     for key in keys:
-#         if key in welcome:
-#             return key
-#     else:
-#         print("No matching keywords, please select one of the following keywords: " + str(keys))
-#         category = input("Please select a category. ")
-#         return category
-
-
-# def choose_ability():
-#     ability = input("Choose an Ability: ").lower().replace(" ", "-")
-#     response = requests.get(URL + "ability").json()
-
-#     # search through the list of abilities
-#     for result in response["results"]:
-#         if result["name"] == ability:
-#             ability_url = result["url"]
-#             break
-#     else:
-#         print("Invalid ability name.")
-#         choose_ability()
-#         return
-
-#     response = requests.get(ability_url).json()
-#     print("Name:", response["name"])
-#     for entry in response["effect_entries"]:
-#         if entry["language"]["name"] == "en":
-#             print("Entry entry: " + entry["effect"])
-#             break
-#         else:
-#             print("No effect in English found for this ability.")
-
-#     for entry in response["effect_changes"]:
-#         for effect_entry in entry["effect_entries"]:
-#             if effect_entry["language"]["name"] == "en":
-#                 print(effect_entry["effect"])
-#                 break
-#         else:
-#             print("No effect change in English found for this ability.")
-    
-#     for entry in response["flavor_text_entries"]:
-#         if entry["language"]["name"] == "en":
-#             print(entry["flavor_text"])
-#             break
-#     print("Pokemon who can have the ability ")
-#     for entry in response["pokemon"]:
-#         if entry:
-#             print(entry["pokemon"]["name"])
-
-
-# def choose_berry():
-#     berry = input("Choose a Berry: ").lower()
-#     response = requests.get(URL + "berry").json()
-
-#     # Search through the list of berries for the one the user chose
-#     for result in response["results"]:
-#         print(result)
-#         if result["name"] == berry:
-#             berry_url = result["url"]
-#             break
-#     else:
-#         print("Invalid berry name.")
-#         choose_berry()
-#         return
-
-#     response = requests.get(berry_url).json()
-#     print(response.effect_changes.json())
-
-
-# def choose_pokemon():
-#     # url is pokemon based abd not number based. Can take pokemon from input
-#     pokemon = input("Choose a pokemon: ").lower()
-#     new_pokemon_url = URL + "pokemon/" + pokemon
-#     response = requests.get(new_pokemon_url).json()
-#     print(response)
-
-# def choose_move():
-#     move = input("Choose a move: ").lower().replace(" ", "-")
-#     response = requests.get(URL + "move").json()
-
-#     for result in response["results"]:
-#         if result["name"] == move:
-#             move_url = result["url"]
-#             break
-#     else:
-#         print("Invalid move name.")
-#         choose_move()
-
-#     response = requests.get(move_url).json()
-#     print(response)
-
-
-# def main():
-#     while True:
-#         if input != "exit":
-#             category = overview()
-#             if category == "pokemon":
-#                 choose_pokemon()
-#             elif category == "ability":
-#                 choose_ability()
-#             elif category == "berry":
-#                 choose_berry()
-#             elif category == "item":
-#                 choose_item()
-#             elif category == "move":
-#                 choose_move()
-#             else:
-#                 print("Unknown category")
-#         else:
-#             break
-
-
-# if __name__ == "__main__":
-#     main()
-
-
 import requests
 import PySimpleGUI as sg
 
 URL = "https://pokeapi.co/api/v2/"
+
 
 def overview():
     layout = [[sg.Text("Using a keyword, what are you looking for?")],
@@ -159,7 +28,8 @@ def overview():
                 window.close()
                 return key, False  # indicate that a valid option has been selected
 
-        sg.popup("No matching keywords, please select one of the following keywords: " + str(keys))
+        sg.popup(
+            "No matching keywords, please select one of the following keywords: " + str(keys))
         category = sg.popup_get_text("Please select a category. ")
         if category is None:  # user clicked Cancel on the popup
             window.close()
@@ -167,6 +37,7 @@ def overview():
         else:
             window.close()
             return category, False  # indicate that a valid option has been selected
+
 
 def choose_ability():
     layout = [[sg.Text("Choose an Ability:")],
@@ -261,18 +132,42 @@ def choose_item():
         else:
             sg.popup("Invalid Item name.")
             continue
-        
+
         response = requests.get(item_url).json()
 
-        name = response["name"]
+        attributes = [result["name"] for result in response["attributes"]]
+        attributes_str = ", ".join(attributes)
 
+        category_name = response["category"]["name"]
+
+        for entry in response["effect_entries"]:
+            if entry["language"]["name"] == "en":
+                effect = entry["effect"]
+                short_effect = entry["short_effect"]
+                break
+
+        for entry in response["flavor_text_entries"]:
+            if entry["language"]["name"] == "en":
+                flavor_text = entry["text"]
+                break
+
+        cost = response["cost"]
         window.close()
-        layout = [[sg.Text("Name: " + name.replace(" ", "-"))],
-                    [sg.Button("OK")]]
+        name = response["name"]
+        layout = [[sg.Text("Name: " + name.replace(" ", "-").capitalize())],
+                  [sg.Text("Cost: " + str(cost))],
+                  [sg.Text("Description: \n" + flavor_text)],
+                  [sg.Text("Attributes: " + "".join(attributes_str))],
+                  [sg.Text("Category: " +
+                           category_name.replace(" ", "-").capitalize())],
+                  [sg.Text("Effect Entry: \n" + effect)],
+                  [sg.Text("Short Effect: \n" + short_effect)],
+                  [sg.Button("OK")]]
         window = sg.Window(name, layout, finalize=True)
         window.TKroot.focus_force()
         event, values = window.read()
         window.close()
+
 
 def choose_berry():
     berry = input("Choose a Berry: ").lower()
@@ -300,6 +195,7 @@ def choose_pokemon():
     response = requests.get(new_pokemon_url).json()
     print(response)
 
+
 def choose_move():
     move = input("Choose a move: ").lower().replace(" ", "-")
     response = requests.get(URL + "move").json()
@@ -316,8 +212,6 @@ def choose_move():
     print(response)
 
 
-
-
 def main():
     while True:
         category, exit_app = overview()
@@ -328,6 +222,10 @@ def main():
             choose_ability()
         elif category == "item":
             choose_item()
+        elif category == "pokemon":
+            choose_pokemon()
+        elif category == "berry":
+            choose_berry()
 
     sg.popup("Goodbye!")
 
